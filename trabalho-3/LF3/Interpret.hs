550b0f063498f5c530d64dd2ee28fa814fa455c3
hrroc <99548553+hrrocha0@users.noreply.github.com>
2025-10-31 08:48:44 -0300

    Trabalho 3 - Atualização do Interpret.hs

--- trabalho-3/LF3/Interpret.hs
+++ trabalho-3/LF3/Interpret.hs
@@ -10,27 +10,37 @@ import Interpreter
 import Optimizer
 import Typechecker
 
+-- TODO: explicar a refatoração de Interpret.hs
+
 main :: IO ()
 main = do
   interact calc
   putStrLn ""
 
 calc :: [Char] -> String
-calc soureCode =
-  let parserResult = pProgram (myLexer soureCode)
-   in case parserResult of
-        Ok ast ->
-          let typeCheckResult = typeCheckP ast
-           in if any isError typeCheckResult
-                then show (filter isError typeCheckResult)
-                else
-                  let optProgram = optimizeP ast
-                   in ">>>>>>> Programa original:<<<<<<< \n"
-                        ++ printTree ast
-                        ++ "\n"
-                        ++ ">>>>>>> Programa otimizado:<<<<<<< \n"
-                        ++ printTree optProgram
-                        ++ "\n"
-                        ++ ">>>>>>> Resultado da execucao:<<<<<<< \n"
-                        ++ show (executeP optProgram)
-        Bad erorMessage -> erorMessage
+calc sourceCode =
+  case runProgram sourceCode of
+    Ok (ast, opt, rv) -> showProgram ast opt rv
+    Bad errorMessage -> errorMessage
+
+runProgram :: [Char] -> Err (Program, Program, Valor)
+runProgram sourceCode = do
+  ast <- pProgram (myLexer sourceCode)
+  opt <-
+    let typeCheckResult = typeCheckP ast
+     in if any isError typeCheckResult
+          then Bad (show $ filter isError typeCheckResult)
+          else Ok (optimizeP ast)
+
+  return (ast, opt, executeP opt)
+
+showProgram :: Program -> Program -> Valor -> String
+showProgram ast opt rv =
+  ">>>>>>> Programa original:<<<<<<< \n"
+    ++ printTree ast
+    ++ "\n"
+    ++ ">>>>>>> Programa otimizado:<<<<<<< \n"
+    ++ printTree opt
+    ++ "\n"
+    ++ ">>>>>>> Resultado da execucao:<<<<<<< \n"
+    ++ show rv
